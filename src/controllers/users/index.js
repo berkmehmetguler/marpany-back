0075e9448a19bd3f56ab74f059270fe0441c78fa
Serkan Çağman <serkancagmann@gmail.com>
2023-07-12 11:45:35 +0300

    Add:reset password

--- src/controllers/users/index.js
+++ src/controllers/users/index.js
@@ -4,7 +4,9 @@ import { userValidation } from "./validation.js";
 import { signAccessToken, signRefreshToken } from "../../middleware/jwt.js";
 import { sendMailVerification } from "../../utils/mail_sender/sendEmail.js";
 import { verificationTemplate } from "../../utils/mail_sender/templates/verification/verification.js";
+import { forgotPasswordTemplate } from "../../utils/mail_sender/templates/forgot_password/forgot_password.js";
 import crypto from "crypto";
+import bcrypt from "bcrypt";
 const createUser = async (req, res) => {
   const { phone, email_address, username } = req.body;
 
@@ -54,17 +56,15 @@ const createUser = async (req, res) => {
 
 const verifyEmail = async (req, res) => {
   const { id, token } = req.params;
-  console.log('id', id);
-    console.log('token', token);
   try {
     const isExistToken = await Token.findOne({
       _id: id,
       token,
       token_type: "verification",
     });
-    console.log('isExistToken', isExistToken);
+    console.log("isExistToken", isExistToken);
     if (!isExistToken) {
-        console.log("Invalid token");
+      console.log("Invalid token");
       return res.status(404).json({ error: "Invalid token" });
     }
 
@@ -84,40 +84,105 @@ const verifyEmail = async (req, res) => {
   }
 };
 
-const forgetPassword = async (req, res, next) => {
-    const { user_info } = req.body;
-  
-    try {
-      const user = await User.findOne(
-        { $or: [{ email: user_info }, { username: user_info }] },
-      );
-  
-      if (!user) {
-        return res.status(200).json({ message: "Success" });
-      }
-      const tokenIsExist = await Token.findOne({ userId: user._id, token_type: "forgetPassword" });
-      if (tokenIsExist) {
-        await Token.deleteOne({ userId: user._id });
-      }
-  
-      const token = await new Token({
-        userId: user._id,
-        token: crypto.randomBytes(32).toString("hex"),
-        token_type: "forgetPassword",
-      }).save();
-  
-      const url = `${process.env.BASE_URL}app/auth/resetPassword/rf&scr/${user._id}/token/${token.token}`;
-      const mailData = {
-        email: user.email,
-        subject: "Reset Password",
-        body: url,
-      };
-  
-      sendMailVerification(mailData);
-      res.status(200).json({ message: "Success" });
-    } catch (e) {
-      next(e);
-    }
-  };
+const forgotPassword = async (req, res, next) => {
+  const { user_info } = req.body;
 
-export { createUser, verifyEmail}
+  try {
+    const user = await User.findOne({
+      $or: [{ email_address: user_info }, { username: user_info }],
+    });
+
+    if (!user) {
+      return res.status(200).json({ message: "Success" });
+    }
+    const tokenIsExist = await Token.findOne({
+      userId: user._id,
+      token_type: "forgot_password",
+    });
+    if (tokenIsExist) {
+      await Token.deleteOne({ userId: user._id });
+    }
+
+    const token = await new Token({
+      userId: user._id,
+      token: crypto.randomBytes(32).toString("hex"),
+      token_type: "forgot_password",
+    }).save();
+
+    const url = `${process.env.BASE_URL}/auth/reset_password?virtualize=${user._id}&token=${token.token}`;
+    const mailData = {
+      email: user.email_address,
+      subject: "Reset Password",
+      body: forgotPasswordTemplate(user.first_name, url),
+    };
+
+    sendMailVerification(mailData);
+    res.status(200).json({ message: "Success" });
+  } catch (e) {
+    next(e);
+  }
+};
+
+const resetPassword = async (req, res, next) => {
+  const { id, token } = req.params;
+  const { password } = req.body;
+  try {
+    const user = await User.findOne({ _id: id });
+    if (!user) {
+      return res.status(400).json({message:"user not found"});
+    }
+
+    const isOldPassword = await bcrypt.compare(password, user.password);
+    if (isOldPassword) {
+      return res.status(400).json({message:"new password must be different from old password"});
+    }
+
+    const tokenData = await Token.findOne({
+      userId: user._id,
+      token,
+      token_type: "forgot_password",
+    });
+    if (!tokenData) {
+      return res.status(400).json({message:"invalid link"});
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(password, salt);
+
+    await User.updateOne({ _id: id }, { $set: { password: hashPassword } });
+    await Token.deleteOne({
+      userId: user._id,
+      token,
+      token_type: "forgot_password",
+    });
+
+    res.json({
+      message: "password has been reset",
+    });
+  } catch (e) {
+    next(e);
+  }
+};
+
+const checkToken = async (req, res, next) => {
+  const { id, token } = req.params;
+  const { token_type } = req.body;
+
+  try {
+    const isExist = await Token.findOne({
+      token,
+      userId: id,
+      token_type,
+    });
+
+    if (!isExist) {
+      return res.status(400).json({ message: "Invalid token" });
+    }
+
+    res.status(201).json({ message: "Success" });
+  } catch (e) {
+    next(e);
+  }
+};
+
+export { createUser, verifyEmail, forgotPassword, resetPassword, checkToken };
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
-import { createUser,verifyEmail } from "../controllers/users/index.js";
+import { createUser,verifyEmail,forgotPassword,resetPassword,checkToken} from "../controllers/users/index.js";
 
 const router = Router();
 
 router.post("/signup", createUser);
 router.get("/verify_email/:id/:token", verifyEmail);
+router.post("/forgot_password", forgotPassword);
+router.post("/reset_password/:id/:token", resetPassword);
+router.post("/check_token/:id/:token", checkToken);
 export default router;
